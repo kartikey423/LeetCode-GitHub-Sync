afe7667439452bf5a6497cffbec61d8f80ed8079
KARTIKEY PURI <62470425+kartikey423@users.noreply.github.com>
2026-07-17 13:11:53 +0530

    Time: 39 ms (78.67%) | Memory: 125.5 MB (74.67%) - LeetSync

--- 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
@@ -1,26 +1,39 @@
 class Solution {
-    using ll = long long;
 public:
-    vector<int> gcdValues(vector<int>& A, vector<long long>& queries) {
-        int mx = ranges::max(A);
-        vector<int> freq(mx + 1, 0);
-        vector<ll> GCD(mx + 1, 0);
-        
-        for (auto& a : A) freq[a]++;
-        
-        for (int i = mx; i > 0; i--) {
-            ll sm = 0, extra = 0;
-            for (int j = i; j <= mx; j += i)
-                sm += freq[j], extra += GCD[j];
-            GCD[i] = sm * (sm - 1) / 2 - extra;
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        long long int mx = *max_element(nums.begin(), nums.end());
+
+        vector<int> mp(mx + 1, 0);
+        for (auto it : nums) {
+            mp[it]++;
         }
-        
-        partial_sum(GCD.begin(), GCD.end(), GCD.begin());
-        
-        vector<int> res(queries.size());
-        for (int i = 0; i < queries.size(); i++)
-            res[i] = ranges::upper_bound(GCD, queries[i]) - GCD.begin();
-            
-        return res;
+
+        vector<long long> cnt(mx + 1, 0);
+
+        for (int i = mx; i >= 1; i--) {
+            long long curr = 0;
+
+            for (int k = i; k <= mx; k += i) {
+                curr += mp[k]; // inclusion of all possible pairs
+            }
+
+            cnt[i] = curr * (curr - 1) / 2;
+
+            for (int k = 2 * i; k <= mx; k += i) {
+                cnt[i] -= cnt[k]; // exclusion of all pairs covered by greater gcd
+            }
+        }
+
+        for (int i = 1; i <= mx; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        vector<int> sol;
+
+        for (auto it : queries) {
+            sol.push_back(lower_bound(cnt.begin(), cnt.end(), it + 1) - cnt.begin());
+        }
+
+        return sol;
     }
 };
